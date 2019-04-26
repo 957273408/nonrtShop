dc43b8af182768b8fc118f95449d7be4af631da8
aaron <957273408@qq.com>
2019-04-26 21:22:12 +0800

    专题页面

--- pages/topic/topic.js
+++ pages/topic/topic.js
@@ -1,66 +1,122 @@
-// pages/topic/topic.js
+let util = require('../../utils/util.js')
+let api = require('../../config/api.js')
+
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    TopicList: [],
+    scrollTop: 0,
+    page: 1,
+    size: 10,
+    count: 0,
+    showPage: false
   },
 
+  // 下一页方法
+  nextPage(){
+    if(this.data.page+1>this.data.count/this.data.size){
+      return 
+    }
+    this.setData({
+      page: parseInt(this.data.page)+1
+    })
+    this.getTopic()
+  },
+  prevPage(){
+    if(this.data.page<2){
+      return 
+    }
+    this.setData({
+      page: parseInt(this.data.page)-1
+    })
+    this.getTopic()
+  },
+
+  getTopic() {
+    this.setData({
+      scrollTop: 0,
+      showPage: false,
+      TopicList: []
+    })
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 2000
+    })
+    util.request(api.TopicList, {
+      page: this.data.page,
+      size: this.data.size
+    }).then(res => {
+      console.log(res)
+      if (res.errno === 0) {
+        this.setData({
+          TopicList: res.data.data,
+          count: res.data.count,
+          showPage: true,
+          scrollTop: 0
+        })
+      }
+      wx.hideToast() //隐藏提示框
+    })
+
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.getTopic()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/topicDetail/topicDetail.js
+++ pages/topicDetail/topicDetail.js
@@ -1,66 +1,101 @@
-// pages/topicDetail/topicDetail.js
+let app=getApp()
+let Wxparse=require('../../lib/wxParse/wxParse.js')
+let util =require('../../utils/util.js')
+let api = require('../../config/api.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    id:'',
+    topicList:[],
+    topic:[],
+    commentList:[],
+    commentCount:'',
   },
+
+  getCommentList(){
+    util.request(api.CommentList, { valueId: this.data.id, typeId: 1, size: 5 }).then(res=>{
+      if(res.errno!=0) return
+      console.log(res)
+      this.setData({
+        commentList: res.data.data,
+        commentCount: res.data.count
+      })
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 页面初始化 options为页面跳转所带来的参数
+    this.setData({
+      id:parseInt(options.id)
+    })
+    util.request(api.TopicDetail,{id:this.data.id}).then(res=>{
+      if(res.errno!=0) return
+      this.setData({topic:res.data})
+      Wxparse.wxParse('topicDetail', 'html', res.data.content,this)
+    })
+    util.request(api.TopicRelated,{id:this.data.id}).then(res=>{
+      console.log(res)
+      if(res.errno!=0) return
+      this.setData({
+        topicList:res.data
+      })
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.getCommentList()
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
   }
 })
